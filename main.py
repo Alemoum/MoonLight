846fe847ca8c195263488225b522b24fb0ef2702
Alemoum <marcosvmm3110@gmail.com>
2025-04-23 18:46:43 -0300

    Drawing the character

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import pygame
 from constants import *
-
+from player import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     time_control = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
+    
+    #GAME LOOP
     while True:
+        #Exit button
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        #Drawing the character
         screen.fill(color=000,rect=None,special_flags=0)
+        player.draw(screen)
+        
+        #Control FPS
         pygame.display.flip()
         time_control.tick(60)
         dt = time_control.tick(60) / 1000
-    # print("Starting Asteroids!")
-    # print("Screen width: 1280")
-    # print("Screen height: 720")
+        
+    
     
 if __name__ == "__main__":
     main()
